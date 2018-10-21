969994f0a721c013584ba6b7718370fe883f9b8b
GXCHAIN <287590296@qq.com>
2018-10-21 18:29:38 +0800

    v1.1 protect private data

--- main/main.go
+++ main/main.go
@@ -6,8 +6,8 @@ import (
 )
 
 func main() {
-	client := merchantClient.NewMerchantClient("5K8iH1jMJxn8TKXXgHJHjkf8zGXsbVPvrCLvU2GekDh2nk4ZPSF", "1.2.323", "http://192.168.1.124:6388")
-	params := map[string]interface{}{"bankCardNo": "6236681540015259109"}
+	client := merchantClient.NewMerchantClient("5K8iH1jMJxn8TKXXgHJHjkf8zGXsbVPvrCLvU2GekDh2nXXXXXX", "1.2.323", "http://192.168.1.124:XXXX")
+	params := map[string]interface{}{"bankCardNo": "6236681540015XXXXXX"}
 	createExchangeResult := client.CreateDataExchangeRequest(9, params)
 	response := client.GetResult(createExchangeResult, 8)
 	//fmt.Println(response)
